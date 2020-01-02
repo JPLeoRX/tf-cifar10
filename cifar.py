eeda2a49d29523c123461ee378cd335c742d274c
leo <leo.ertuna@gmail.com>
2020-01-02 22:07:47 +0300

    Even simpler model

--- cifar.py
+++ cifar.py
@@ -58,14 +58,14 @@ def build_model():
         # tf.keras.layers.Dense(256, activation='relu'),
         # tf.keras.layers.Dense(10, activation='softmax')
 
-        tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', input_shape=(32, 32, 3)), tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(12, 3, activation='relu', padding='same', input_shape=(32, 32, 3)), tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D(2, 2), tf.keras.layers.Dropout(0.5),
 
-        tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same'), tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(24, 3, activation='relu', padding='same'), tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D(2, 2), tf.keras.layers.Dropout(0.5),
 
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(48, activation='relu'),
         tf.keras.layers.Dense(10, activation='softmax')
     ])
 
